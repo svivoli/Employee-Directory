087159cea35f6aad935e96b16fc405d8e346f0d3
Sarah Vivoli <snvivoli@gmail.com>
2020-03-13 12:22:43 -0700

    searcharea index.js

---
+++ src/components/SearchArea/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+function SearchArea() {
+    return(
+        <div>
+            <form>
+                <button>DOB</button>
+                <input type="text"></input>
+                <input type="text"></input>
+                <input type="text"></input>
+                <button>Search</button>
+            </form>
+        </div>
+    )
+}
+
+export default SearchArea;
