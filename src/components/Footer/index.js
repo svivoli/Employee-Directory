e37091c5fdd6f6e3c788173ce7288cb311fa7986
Sarah Vivoli <snvivoli@gmail.com>
2020-03-13 12:19:52 -0700

    footer index.js

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <div>
+            Copyright 2020
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import SearchArea from "../SearchArea";
 import Table from "../Table";
-import Heading from "../Heading";
-import DataArea from "../DataArea";
 import "./style.css";
 
 function Main() {
