bc601827a846e29750faaba5b8cf951afc57faaa
Sarah Vivoli <snvivoli@gmail.com>
2020-03-13 11:41:16 -0700

    footer added to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
 import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Wrapper>
           <Main />
         </Wrapper>
+        <Footer />
       </div>
   );
 };
