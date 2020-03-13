b0ddd393b5720811565100bebab9a94294a23509
Sarah Vivoli <snvivoli@gmail.com>
2020-03-13 11:15:42 -0700

    main index.js changed to table from list

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchArea from "../SearchArea";
 import Table from "../Table";
+import Heading from "../Heading";
 import DataArea from "../DataArea";
 import "./style.css";
 
@@ -9,9 +10,12 @@ function Main() {
         <div>
         <SearchArea />
         <Table>
-            <ul>
-                <li><DataArea /></li>
-            </ul>
+            <tr>
+                <th><Heading /></th>
+            </tr>
+            <tr>
+                <td><DataArea /></td>
+            </tr>
         </Table>
         </div>
     );
