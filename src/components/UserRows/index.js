e849f0113bae42c30aa6f8899f66d9a1a00d6669
Sarah Vivoli <snvivoli@gmail.com>
2020-03-22 11:07:24 -0700

    spacing

--- src/components/UserRows/index.js
+++ src/components/UserRows/index.js
@@ -4,19 +4,19 @@ import "./style.css";
 function UserRows(props) {
     console.log(props.data.employees);
     return (
-            <tbody>
-                {Object.keys(props.data.employees).map(index => {
-                    return (
-                        <tr key={index} >
-                            <td><img src={props.data.employees[index].picture.thumbnail} alt="thumbnail"></img></td>
-                            <td>{props.data.employees[index].name.first} {props.data.employees[index].name.last}</td>
-                            <td>{props.data.employees[index].phone}</td>
-                            <td>{props.data.employees[index].email}</td>
-                            <td>{props.data.employees[index].dob.date.split('T')[0]}</td>
-                        </tr>
-                    )
-                })}
-            </tbody>
+        <tbody>
+            {Object.keys(props.data.employees).map(index => {
+                return (
+                    <tr key={index} >
+                        <td><img src={props.data.employees[index].picture.thumbnail} alt="thumbnail"></img></td>
+                        <td>{props.data.employees[index].name.first} {props.data.employees[index].name.last}</td>
+                        <td>{props.data.employees[index].phone}</td>
+                        <td>{props.data.employees[index].email}</td>
+                        <td>{props.data.employees[index].dob.date.split('T')[0]}</td>
+                    </tr>
+                )
+            })}
+        </tbody>
     )
 };
 
