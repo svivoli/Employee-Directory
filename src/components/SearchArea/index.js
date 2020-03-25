024fb0dcfaf1a66ecddd48ffbc2dfe4a7aa80199
Sarah Vivoli <snvivoli@gmail.com>
2020-03-25 09:52:12 -0700

    search bar container, margin, button border

--- src/components/SearchArea/index.js
+++ src/components/SearchArea/index.js
@@ -2,17 +2,19 @@ import React from "react";
 import "./style.css";
 
 function SearchArea({ handleSearch }) {
-    return(
-        <div className="input-group">
-                <input 
-                className="form-control"
-                type="text"
-                onChange={event => handleSearch(event)}
-                placeholder="Search by Employee Name"
+    return (
+        <div className="container">
+            <div className="input-group">
+                <input
+                    className="form-control"
+                    type="text"
+                    onChange={event => handleSearch(event)}
+                    placeholder="Search by Employee Name"
                 />
                 <span className="input-group-btn">
-                    <button className="btn btn-default" type="button">Search</button>
+                    <button className="btn" type="button">Search</button>
                 </span>
+            </div>
         </div>
     )
 }
